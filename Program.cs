b2c4716e028ff1a8d2162a7ba1dcf10c4e7afc2c
mein9 <mateo0221@gmail.com>
2024-07-04 11:25:24 -0700

    finished homework

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Matt");
+            Console.WriteLine("I was in the army as an Electromagnetic Spectrum Manager");
+            PartTwo();
+           
+
+        }
+        static void PartTwo()
+        {
+            Console.WriteLine("  ^  ");
+            Console.WriteLine(" ^^^ ");
+            Console.WriteLine("^^^^^");
+        }
+
+    }
+}
